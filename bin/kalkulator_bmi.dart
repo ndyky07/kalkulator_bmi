058a1843900405986a6d8900aa6dea535f10e8dd
ndyky07 <sandipamungkas600@gmail.com>
2025-09-30 18:07:17 +0700

    first commit

---
+++ bin/kalkulator_bmi.dart
@@ -0,0 +1,5 @@
+import 'package:kalkulator_bmi/kalkulator_bmi.dart' as kalkulator_bmi;
+
+void main(List<String> arguments) {
+  print('Hello world: ${kalkulator_bmi.calculate()}!');
+}
---
+++ test/kalkulator_bmi_test.dart
@@ -0,0 +1,8 @@
+import 'package:kalkulator_bmi/kalkulator_bmi.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
